55b45f4c2ae806036076d586b7cd437226b423d9
oleolerato <olerato.mosikidi@gmail.com>
2022-07-11 13:36:37 +0200

    Add code for computerSelection to get random value from compOption array, as well as Add function for game

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
 function computerPlay() {
+  const compOptions = ['Rock'
+  , 'Paper'
+  ,'Scissors'];
+  let randomGen = Math.floor(Math.random() * compOption.length);
+}
+
+computerPlay();
+
+function game() {
   
 }
