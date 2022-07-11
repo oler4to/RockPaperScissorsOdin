18053860ff9acea8fda518b744a2a502ef50c276
oleolerato <olerato.mosikidi@gmail.com>
2022-07-11 16:18:03 +0200

    Create playerSelection variable in 'game' function and add conditional for game

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 
-function computerPlay() {
-  const compOptions = ['Rock'
+  let computerSelection = ['Rock'
   , 'Paper'
   ,'Scissors'];
-  let randomGen = Math.floor(Math.random() * compOption.length);
+  
+function computerPlay() {
+  let randomGen = Math.floor(Math.random() * computerSelection.length);
+  
+  if(randomGen == 0){
+    computerSelection = "Rock";
+  } else if(randomGen == 1){
+    computerSelection = "Paper";
+  } else if(randomGen == 2){
+    computerSelection = "Scissors";
+  }
 }
 
 computerPlay();
 
 function game() {
+  let playerSelection = prompt("Pick your fighter");
   
+  if(playerSelection == computerSelection){
+    console.log("THAT'S A TIE");
+    
+  } else if((playerSelection == "Rock" && computerSelection == Paper) 
+  || (playerSelection == "Paper" && computerSelection == "Scissors" ) 
+  || (playerSelection == "Scissors" && computerSelection == "Rock") ){
+    console.log("THAT'S ONE FOR THE COMPUTER");
+    
+  } else{
+    console.log("WELL DONE!")
+  }
 }
+game();
