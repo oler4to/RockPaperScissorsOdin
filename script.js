411034256ec6fc1b9a5291a7c1594eb937d1ba5d
oleolerato <olerato.mosikidi@gmail.com>
2022-07-22 21:40:00 +0200

    Declare 'computerSelection' variables with Array

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const buttons = document.querySelector('button');
 const playResult = document.querySelector('.result');
 const score = document.querySelector('.score');
 const endResult = document.querySelector('.endResult');
+
+const computerSelection = ["Rock", "Paper", "Scissors"].toLowerCase();
