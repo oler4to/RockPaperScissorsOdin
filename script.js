8fce3af2f32778bbc00ec242942af21d8372afce
oleolerato <olerato.mosikidi@gmail.com>
2022-07-22 21:35:56 +0200

    Target all the 'div', 'button' and 'p' elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const game = document.querySelector('#game');
+const buttons = document.querySelector('button');
+
+const playResult = document.querySelector('.result');
+const score = document.querySelector('.score');
+const endResult = document.querySelector('.endResult');
