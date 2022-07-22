3301f4b0eb62e55c8708d9ee3f6a5d96a64769a4
oleolerato <olerato.mosikidi@gmail.com>
2022-07-22 22:08:56 +0200

    Add 2 conditions to change the 'computerSelection' with either Paper or Scissors depending on the random variable

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ function computerPlay(){
   let random = (Math.floor(Math.random() * computerSelection.length));
   
   if (random == 0){
-    computerSelection = "Rock"
+    computerSelection = "Rock";
+  } else if (random == 1){
+    computerSelection = "Paper";
+  } else if(random == 2){
+    computerSelection = "Scissors";
   }
   
-  console.log(computerSelection)
 }
 
 computerPlay()
