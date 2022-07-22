edf54603c4c0cdfb8e8f55c3041e27511798bef0
oleolerato <olerato.mosikidi@gmail.com>
2022-07-22 21:53:13 +0200

    Create a function that generates a random number from the length of the computerSelection

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const playResult = document.querySelector('.result');
 const score = document.querySelector('.score');
 const endResult = document.querySelector('.endResult');
 
-const computerSelection = ["Rock", "Paper", "Scissors"].toLowerCase();
+const computerSelection = ["Rock", "Paper", "Scissors"];
+
+function computerPlay(){
+  let random = (Math.floor(Math.random() * computerSelection.length));
+  
+  console.log(random);
+}
+
+computerPlay()
